20a68b41b0222a7693cabc531065c0de9ea36a97
Isaac Dickinson <sun@veriny.tf>
2016-10-30 20:07:01 +0000

    Add memory usage display to stats command.

--- joku/cogs/core.py
+++ joku/cogs/core.py
@@ -3,12 +3,11 @@ Core commands.
 """
 import inspect
 
-import asyncio
-
 import discord
 from discord.ext import commands
 from discord.ext.commands import Command, CheckFailure
 from discord.ext.commands import Context
+import psutil
 
 from joku.bot import Jokusoramame
 from joku.checks import is_owner
@@ -66,19 +65,23 @@ class Core(object):
         """
         Shows stats about the bot.
         """
+        current_process = psutil.Process()
+
         tmp = {
             "shards": ctx.bot.manager.max_shards,
             "servers": sum(1 for _ in ctx.bot.manager.get_all_servers()),
             "members": sum(1 for _ in ctx.bot.manager.get_all_members()),
             "unique_members": ctx.bot.manager.unique_member_count,
             "channels": sum(1 for _ in ctx.bot.manager.get_all_channels()),
-            "shard": ctx.bot.shard_id
+            "shard": ctx.bot.shard_id,
+            "memory": (current_process.memory_info().rss / 1024 // 1024)
         }
 
         await ctx.bot.say("Currently connected to `{servers}` servers, "
                           "with `{channels}` channels "
                           "and `{members}` members (`{unique_members}` unique) "
-                          "across `{shards}` shards.\n\n"
+                          "across `{shards}` shards.\n"
+                          "Currently using **{memory}MB** of memory\n\n"
                           "This is shard ID **{shard}**.".format(**tmp))
 
     @commands.command(pass_context=True)
