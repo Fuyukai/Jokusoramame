f2095c298a83544e570de0981f2f37f6b09fca7e
SunDwarf <sun@veriny.tf>
2016-11-21 22:07:55 +0000

    mystery error tracking pt1

--- joku/manager.py
+++ joku/manager.py
@@ -111,6 +111,13 @@ class SingleLoopManager(object):
         if fut is None:
             self.logger.info("Loading shard {}...".format(shard_id))
         else:
+            try:
+                res = fut.result()
+            except Exception as e:
+                self.logger.exception()
+            else:
+                self.logger.info("Shard {} returned {}".format(res, shard_id))
+
             self.logger.info("Reloading shard {}...".format(shard_id))
         bot = Jokusoramame(self.config, shard_id=shard_id, shard_count=shard_count,
                            manager=self, state=SharedState)
