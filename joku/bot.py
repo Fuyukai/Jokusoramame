49d7aa4872b432c7737c94c5255af536bbf28b8c
SunDwarf <sun@veriny.tf>
2016-12-10 20:22:57 +0000

    Commands are now triggered on message edit.

--- joku/bot.py
+++ joku/bot.py
@@ -242,6 +242,9 @@ class Jokusoramame(Bot):
 
         await super().on_message(message)
 
+    async def on_message_edit(self, before: discord.Message, message: discord.Message):
+        await self.on_message(message)
+
     def run(self):
         token = self.config["bot_token"]
         super().run(token)
