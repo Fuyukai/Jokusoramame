81a088e8ba599d21932f4156815a66e048c6ecad
Laura <l@veriny.tf>
2017-01-31 17:38:14 +0000

    Add bad raffle command.

--- joku/cogs/games/currency.py
+++ joku/cogs/games/currency.py
@@ -9,6 +9,18 @@ from joku.cogs._common import Cog
 from joku.redis import with_redis_cooldown
 
 
+BAD_RESPONSES = [
+    ":fire: Your bank account went up in flames and you lost `§{}`.",
+    ":grapes: You spend too much in the supermarket and you lost `§{}`.",
+    ":spider: A spider arrives and you get so spooked you drop `§{}`.",
+]
+
+GOOD_RESPONSES = [
+    ":money_mouth: You exploit the working class and gain `§{}`.",
+    ":medal: You win first place in the Money Making Race and gain `§{}`.",
+    ":slot_machine: You have a gambling addiction and win `§{}`.",
+]
+
 class Currency(Cog):
     @commands.command(pass_context=True)
     @with_redis_cooldown(bucket="daily_currency")
@@ -20,6 +32,32 @@ class Currency(Cog):
 
         await ctx.bot.rethinkdb.update_user_currency(ctx.message.author, amount)
         await ctx.channel.send(":money_with_wings: **You have earned `§{}` today.**".format(amount))
+
+    @commands.command(pass_context=True)
+    @with_redis_cooldown(bucket="hourly_raffle", type_="HOURLY")
+    async def raffle(self, ctx: Context):
+        """
+        Will you win big or will you lose out?
+        """
+        currency = await ctx.bot.rethinkdb.get_user_currency(ctx.message.author)
+        if currency <= 0:
+            addiction = """Need help with a gambling addiction? We're here to help.
+
+UK: <http://www.gamcare.org.uk/>
+US: <http://www.ncpgambling.org/>
+Canada: <https://www.problemgambling.ca/Pages/Home.aspx>"""
+            await ctx.send(addiction)
+            return
+
+        amount = self.rng.randint(-600, 300)
+
+        await ctx.bot.rethinkdb.update_user_currency(ctx.message.author, amount)
+        if amount < 0:
+            choice = self.rng.choice(BAD_RESPONSES)
+        else:
+            choice = self.rng.choice(GOOD_RESPONSES)
+
+        await ctx.send(choice.format(amount))
 
     @commands.group(pass_context=True, invoke_without_command=True)
     async def store(self, ctx: Context):
