ddab64c4e000b74c5a512060df33041cd1bbc55d
Laura <l@veriny.tf>
2017-02-07 18:25:18 +0000

    Add redirect command to redirect messages to a different channel.
    Signed-off-by: Laura <l@veriny.tf>

--- joku/cogs/misc.py
+++ joku/cogs/misc.py
@@ -9,6 +9,7 @@ from asyncio_extras import threadpool
 from discord.ext import commands
 
 from joku.bot import Context
+from joku.checks import has_permissions
 from joku.cogs._common import Cog
 
 
@@ -41,6 +42,35 @@ class Misc(Cog):
         m = await ctx.send(":heavy_check_mark: Cleaned up `{}` messages.".format(count))
         await asyncio.sleep(5)
         await m.delete()
+
+    @commands.command()
+    @has_permissions(manage_messages=True)
+    async def redirect(self, ctx: Context, channel: discord.TextChannel, message: int):
+        """
+        Redirects a message to another channel.
+
+        The message argument passed must be a message ID.
+        To get a message ID you should right click on the on a message and then click "Copy ID". You must have Developer
+        Mode enabled to get that functionality.
+        """
+        o_chan = ctx.channel  # type: discord.TextChannel
+        try:
+            msg = await o_chan.get_message(message)
+        except discord.HTTPException:
+            await ctx.send(":x: Could not find that message.")
+            return
+
+        # delete it and copy it to the target channel
+        em = discord.Embed(title="Redirected by {}".format(ctx.author.name))
+        em.description = msg.content
+        em.set_author(name=msg.author.name, icon_url=msg.author.avatar_url)
+        em.timestamp = msg.created_at
+        await msg.delete()
+        await ctx.message.delete()
+        await channel.send("{}: This message was redirected here from {}:"
+                           .format(msg.author.mention, ctx.channel.mention),
+                           embed=em)
+
 
 
 setup = Misc.setup
