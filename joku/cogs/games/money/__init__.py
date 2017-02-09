7006d978ffa739594465c616210500c0fe0c66e0
bippum <brendan10211@gmail.com>
2017-02-09 13:44:57 -0500

    lol

--- joku/cogs/games/money/__init__.py
+++ joku/cogs/games/money/__init__.py
@@ -18,7 +18,8 @@ BAD_RESPONSES = [
     ":fire: Your bank account went up in flames and you lost `§{}`.",
     ":grapes: You spend too much in the supermarket and you lost `§{}`.",
     ":spider: A spider arrives and you get so spooked you drop `§{}`.",
-    ":hammer_pick: The revolution comes and your wealth of `§{}` is redistributed."
+    ":hammer_pick: The revolution comes and your wealth of `§{}` is redistributed.",
+    ":dragon: Ryuu ga waga teki wo kurau! (You lost §{}.)"
 ]
 
 GOOD_RESPONSES = [
