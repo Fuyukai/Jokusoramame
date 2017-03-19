6282d27839582b6e1c39d988e334cf025a0587dd
Laura <l@veriny.tf>
2017-03-19 11:56:57 +0000

    Add some map commands.
    Signed-off-by: Laura <l@veriny.tf>

--- joku/cogs/location.py
+++ joku/cogs/location.py
@@ -2,12 +2,15 @@
 Cog for interfacing with Google Maps and similar.
 """
 import datetime
+from io import BytesIO
 from urllib.parse import urlencode
 
+import aiohttp
 import discord
 import functools
 import googlemaps
 import pprint
+from bingmaps.apiservices import TrafficIncidentsApi
 from discord.ext import commands
 from discord.ext.commands import BucketType
 
@@ -29,11 +32,104 @@ def _sanitize_html_instructions(s: str) -> str:
 
 
 class Location(Cog):
+    MQ_BASE = "https://www.mapquestapi.com"
+    MQ_BETA = "https://beta.mapquestapi.com"
+
+    MQ_MAPS = MQ_BETA + "/staticmap/v5/map"
+
     def __init__(self, bot: Jokusoramame):
         super().__init__(bot=bot)
 
         # Create the google maps client.
         self.maps = googlemaps.Client(key=self.bot.config["maps_api_key"])
+
+    def make_mq_request(self, **kwargs):
+        kwargs["key"] = self.bot.config["mapquest_api_key"]
+
+        return kwargs
+
+    @commands.command()
+    async def map(self, ctx: Context, *, location: str):
+        """
+        Shows a map for the specified area.
+        """
+        # geocode the location
+        async with ctx.channel.typing():
+            geocode_result = await self.bot.loop.run_in_executor(None, self.maps.geocode, location)
+            geocode_result = geocode_result[0]
+            lat = geocode_result["geometry"]["location"]["lat"]
+            long = geocode_result["geometry"]["location"]["lng"]
+
+            req = self.make_mq_request(size="@2x", zoom=12,
+                                       locations="{},{}".format(lat, long),
+                                       traffic="flow|cons|inc")
+
+            async with self.session.get(self.MQ_MAPS, params=req,
+                                        headers={"Accept": "image/png"}) as r:
+                assert isinstance(r, aiohttp.ClientResponse)
+                if r.status != 200:
+                    await ctx.send(":x: Something went wrong.")
+                    raise RuntimeError(await r.text())
+
+                data = BytesIO(await r.read())
+
+        await ctx.send(file=data, filename="map.png")
+
+    @commands.command()
+    async def traffic(self, ctx: Context, *, location: str):
+        """
+        Shows traffic incidents at this location.
+        """
+        # this is broken!
+        return
+        # geocode it using google's API anyway, and then fetch from bing
+        async with ctx.channel.typing():
+            geocode_result = await self.bot.loop.run_in_executor(None, self.maps.geocode, location)
+            geocode_result = geocode_result[0]
+            lat = geocode_result["geometry"]["location"]["lat"]
+            long = geocode_result["geometry"]["location"]["lng"]
+
+            # calculate bounding box
+            # south lat, west long, north lat, east long
+            bounding_box = [lat - 0.1, long - 0.1, lat + 0.1, long + 0.1]
+
+            result = TrafficIncidentsApi(self.make_bing_request(mapArea=bounding_box))
+
+        pprint.pprint(result.traffic_incident())
+
+    @commands.command()
+    async def route(self, ctx: Context, from_: str, to: str):
+        """
+        Shows the route on a map from one place to another.
+        """
+        async with ctx.channel.typing():
+            # do a double google maps geocode
+            geocode_result = await self.bot.loop.run_in_executor(None, self.maps.geocode, from_)
+            geocode_result = geocode_result[0]
+            lat1 = geocode_result["geometry"]["location"]["lat"]
+            long1 = geocode_result["geometry"]["location"]["lng"]
+
+            geocode_result = await self.bot.loop.run_in_executor(None, self.maps.geocode, to)
+            geocode_result = geocode_result[0]
+            lat2 = geocode_result["geometry"]["location"]["lat"]
+            long2 = geocode_result["geometry"]["location"]["lng"]
+
+            req = self.make_mq_request(size="@2x",
+                                       start="{},{}".format(lat1, long1), end="{},{}".format(lat2, long2))
+
+            async with self.session.get(self.MQ_MAPS, params=req,
+                                        headers={"Accept": "image/png"}) as r:
+                assert isinstance(r, aiohttp.ClientResponse)
+                if r.status != 200:
+                    await ctx.send(":x: Something went wrong.")
+                    raise RuntimeError(await r.text())
+
+                data = BytesIO(await r.read())
+
+        qs = urlencode({"saddr": from_, "daddr": to})
+        final = "https://maps.google.com/?" + qs
+
+        await ctx.send(final, file=data, filename="map.png")
 
     @commands.command()
     async def geocode(self, ctx: Context, *, location: str):
